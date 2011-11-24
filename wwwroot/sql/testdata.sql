f18d257eb0cbe175f4a85f1e195eb703bd888663
Antanas Sinica <antanas.sinica@gmail.com>
2011-11-25 00:23:17 +0200

    Rankiniu budu nutrinti failai

--- wwwroot/sql/testdata.sql
+++
@@ -1,5 +0,0 @@
-TRUNCATE TABLE `app_users`;
-
-INSERT INTO `app_users` VALUES (1, "antanas", "password");
-INSERT INTO `app_users` VALUES (2, "raminta", "yy");
-INSERT INTO `app_users` VALUES (3, "tadas", "xx");
