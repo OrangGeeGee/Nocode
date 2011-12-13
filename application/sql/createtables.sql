92bbfd329f36595fb72a19d55509a5d24fb0eec8
tadasleiga <tadas.leiga@gmail.com>
2011-12-13 09:08:12 +0200

    Added unicode charset

--- application/sql/createtables.sql
+++ application/sql/createtables.sql
@@ -10,38 +10,38 @@ CREATE TABLE `app_users` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` varchar(32) NOT NULL,
   `password` varchar(32) NOT NULL
-);
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 CREATE TABLE `app_priemones` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `kodas` varchar(10) NOT NULL,
   `pavadinimas` varchar(255) NOT NULL
-);
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 CREATE TABLE `app_padaliniai` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `kodas` varchar(10) NOT NULL,
   `pavadinimas` varchar(255) NOT NULL
-);
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 CREATE TABLE `app_is` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `kodas` varchar(10) NOT NULL,
   `pavadinimas` varchar(255) NOT NULL
-);
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 CREATE TABLE `app_ispadaliniai` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `iskodas` varchar(10) NOT NULL,
   `padaliniokodas` varchar(19) NOT NULL
-);
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 CREATE TABLE `app_priemonepadaliniai` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `padaliniokodas` varchar(10) NOT NULL,
   `priemoneskodas` varchar(10) NOT NULL,
   `valandos` float NOT NULL DEFAULT 1
-);
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 CREATE TABLE `app_history` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -50,4 +50,4 @@ CREATE TABLE `app_history` (
   `iki` DATE NOT NULL,
   `kiekis` INT NOT NULL,
   `prognoze` BOOLEAN NOT NULL DEFAULT 0
-);
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
